00551ad39340a8336772e047173301412bb382f8
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-04-09 09:54:49 +0530

    get_topheadlines func is created

--- Asyncio/headlines.py
+++ Asyncio/headlines.py
@@ -1,6 +1,14 @@
+from constants_ import NEWS_API
 import requests
+import json
+# newsapi = NewsApiClient(NEWS_API)   
+def get_topheadlines():
+    top_headlines_list = []
+    headlines_url =f"https://newsapi.org/v2/top-headlines?country=in&apiKey={NEWS_API}"
+    res = requests.get(headlines_url)
+    resp = json.loads(res.text)
+    for itm in resp["articles"]:
+        titles = itm["title"]
+        top_headlines_list.append(titles)
+    return top_headlines_list
 
-url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=9bb2a8ecd18543db82f1cd2b19d3e2df"
-
-res = requests.get(url)
-print(res.text)
