9febe93969eb55ff467599ad882a58992e7e1d26
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-04-11 15:19:13 +0530

    getting top headlines fuc is created

---
+++ Tg bot threading/news_funcs.py
@@ -0,0 +1,17 @@
+import json
+import requests
+from const import NEWSURL
+
+
+
+global titles_lst 
+titles_lst = []
+
+def get_news():
+    news_url = f"{NEWSURL}"
+    resp = requests.get(news_url)
+    resp = json.loads(resp.text)
+    for itm in resp["articles"]:
+        titles = itm["title"]  
+        titles_lst.append(titles)
+    return titles_lst
