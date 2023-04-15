a17c6d1a9b99dbf1400a78a79dbeb4838bcdd5db
Kanchan Sharma <kanchan.sharma9722@gmail.com>
2023-04-15 08:35:39 +0530

    fixed - thread was created but was NOT strating

--- Tg bot threading/tg_funcs.py
+++ Tg bot threading/tg_funcs.py
@@ -7,8 +7,9 @@ from threading import Lock
 
 
 global lst_upd_id,q 
-lst_upd_id = 0
+lst_upd_id = 253298223
 q = []
+SENT_COUNT_PER_COMMAND = 0
 
 lock = Lock()
 
@@ -45,27 +46,33 @@ def sender_func():
                 threading.Thread(target=send_all_msgs,args=(chat_id,txt_to_snd),name="send_all_msgs1").start()     
             if usr_txt == "/news":
                 txt_to_snd_lst = get_news() 
-                print(*txt_to_snd_lst)
+                print(f"No. of headlines - {len(txt_to_snd_lst)}")
+                print(f"Message upfate id = {upd_id}")
+                # print(*txt_to_snd_lst)
                 threading.Thread(target=send_all_msgs,args=(chat_id,*txt_to_snd_lst),name="send_all_msgs2").start()
                 
               
 def send_msg(chat_id,txt_to_snd):
+    global SENT_COUNT_PER_COMMAND
     try:
-        print("sending messg..")
+        # print("sending messg..")
         requests.post(f"{TGURL}/sendMessage?chat_id={chat_id}&text={txt_to_snd}")
+        SENT_COUNT_PER_COMMAND += 1
     except Exception as e:
         print(e)
 
 def send_all_msgs(chat_id,*args):
+    global SENT_COUNT_PER_COMMAND
+    SENT_COUNT_PER_COMMAND = 0
     try:
         for msg_to_snd in args:
-            print(f"{TGURL}/sendMessage?chat_id={chat_id}&text={msg_to_snd}")
-            threading.Thread(target=send_msg,args=(chat_id,msg_to_snd),name="send_msg")
+            # print(f"{TGURL}/sendMessage?chat_id={chat_id}&text={msg_to_snd}")
+            threading.Thread(target=send_msg,args=(chat_id,msg_to_snd),name="send_msg").start()
     except Exception as e:
         print(e)
         raise Exception("Error in send_all_mssg")
                 
                 
 if __name__ == "__main__":
-    threading.Thread(target=update_id,name="updater").start()
-    threading.Thread(target=sender_func,name="sendr").start()
+    threading.Thread(target=update_id,name="UPDATE-ID_thread").start()
+    threading.Thread(target=sender_func,name="SENDER-thread").start()
